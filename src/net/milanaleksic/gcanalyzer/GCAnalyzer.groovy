c3ff30403d5fd850324eea5d30125668a1794393
Milan Aleksic <milanaleksic@gmail.com>
2012-01-30 19:16:14 +0100

    support for multiple logs analysis at the same time

--- src/net/milanaleksic/gcanalyzer/GCAnalyzer.groovy
+++ src/net/milanaleksic/gcanalyzer/GCAnalyzer.groovy
@@ -3,6 +3,17 @@ package net.milanaleksic.gcanalyzer
 import javax.swing.JFrame
 import javax.swing.JTabbedPane
 import org.jfree.chart.ChartPanel
+import javax.swing.JPanel
+import javax.swing.JLabel
+import java.awt.BorderLayout
+
+import javax.swing.JButton
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+import javax.swing.JTextField
+import java.awt.GridBagLayout
+import java.awt.GridBagConstraints
+import javax.swing.SwingUtilities
 
 /**
  * User: Milan Aleksic
@@ -12,26 +23,71 @@ import org.jfree.chart.ChartPanel
 class GCAnalyzer {
 
     public static void main(String[] args) {
-        if (args.length == 0) {
-            println "GCAnalyzer should be used by sending a GC output log as parameter"
-            return
-        }
-        new GCAnalyzer().exec(args[0])
+        new GCAnalyzer(args: args).exec()
     }
 
-    public def exec(String file) {
-        def gcEventsInformation = new GCEventsInformation(file)
-        boolean markFullGCs = true
-        JFrame frame = new JFrame('Garbage Collector Log analysis');
+    private JTabbedPane fileTabs
 
-        JTabbedPane tabs = new JTabbedPane();
-        tabs.add('non-PermGen GC', new ChartPanel(gcEventsInformation.getNonPerGenGCChart(markFullGCs)))
-        tabs.add('event timings', new ChartPanel(gcEventsInformation.getEventTimingsChart(markFullGCs)))
-        frame.add(tabs);
-        frame.pack();
-        frame.setExtendedState((int) frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+    private JTextField fileNameTextField
+
+    private String[] args
+
+    public def exec() {
+        JFrame frame = new JFrame('Garbage Collector Log analysis')
+        JPanel panel = new JPanel()
+        def constraints = new GridBagConstraints()
+        panel.setLayout(new GridBagLayout())
+        constraints.gridx=0
+        constraints.fill = GridBagConstraints.HORIZONTAL
+        panel.add(new JLabel("Java GC file: "), constraints)
+        constraints.gridx=1
+        constraints.weightx = 0.8
+        panel.add(fileNameTextField = new JTextField(), constraints)
+        JButton button = new JButton("Load")
+        button.addActionListener( { ActionEvent e ->
+            addFile(fileNameTextField.text)
+        } as ActionListener)
+        constraints.gridx=2
+        constraints.weightx = 0.2
+        panel.add(button, constraints)
+
+        frame.add(panel, BorderLayout.NORTH)
+
+        fileTabs = new JTabbedPane()
+        frame.add(fileTabs)
+        frame.pack()
+        frame.setExtendedState((int) frame.getExtendedState() | JFrame.MAXIMIZED_BOTH)
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+        frame.setVisible(true)
+
+
+        new Thread({
+
+            Thread.sleep(100)
+
+            SwingUtilities.invokeLater {
+                args.each { String filename ->
+                    def file = new File(filename)
+                    if (file.isDirectory()) {
+                        file.eachFile { File childFile ->
+                            addFile(childFile.absolutePath)
+                        }
+                    }
+                    else
+                        addFile(filename)
+                }
+            } as Runnable
+
+        } as Runnable).run()
+    }
+
+    void addFile(String fileName) {
+        def gcEventsInformation = new GCEventsInformation(fileName)
+        JTabbedPane graphTabs = new JTabbedPane()
+        boolean markFullGCs = true
+        graphTabs.add('non-PermGen GC', new ChartPanel(gcEventsInformation.getNonPerGenGCChart(markFullGCs)))
+        graphTabs.add('event timings', new ChartPanel(gcEventsInformation.getEventTimingsChart(markFullGCs)))
+        fileTabs.add(new File(fileName).name, graphTabs)
     }
 
 }
