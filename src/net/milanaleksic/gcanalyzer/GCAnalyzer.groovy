fbe099dfc637275831999daf36a99f97e0196016
Milan Aleksic <milanaleksic@gmail.com>
2012-01-30 19:42:29 +0100

    parallelization of input + can send directory as program parameter

--- src/net/milanaleksic/gcanalyzer/GCAnalyzer.groovy
+++ src/net/milanaleksic/gcanalyzer/GCAnalyzer.groovy
@@ -14,6 +14,8 @@ import javax.swing.JTextField
 import java.awt.GridBagLayout
 import java.awt.GridBagConstraints
 import javax.swing.SwingUtilities
+import groovy.io.FileType
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * User: Milan Aleksic
@@ -26,14 +28,18 @@ class GCAnalyzer {
         new GCAnalyzer(args: args).exec()
     }
 
+    private JFrame frame
     private JTabbedPane fileTabs
-
     private JTextField fileNameTextField
 
     private String[] args
 
+    private AtomicInteger counter = new AtomicInteger(0)
+
+    private static final String TITLE = 'Garbage Collector Log analysis'
+
     public def exec() {
-        JFrame frame = new JFrame('Garbage Collector Log analysis')
+        frame = new JFrame(TITLE)
         JPanel panel = new JPanel()
         def constraints = new GridBagConstraints()
         panel.setLayout(new GridBagLayout())
@@ -41,14 +47,14 @@ class GCAnalyzer {
         constraints.fill = GridBagConstraints.HORIZONTAL
         panel.add(new JLabel("Java GC file: "), constraints)
         constraints.gridx=1
-        constraints.weightx = 0.8
+        constraints.weightx = 0.9
         panel.add(fileNameTextField = new JTextField(), constraints)
         JButton button = new JButton("Load")
         button.addActionListener( { ActionEvent e ->
             addFile(fileNameTextField.text)
         } as ActionListener)
         constraints.gridx=2
-        constraints.weightx = 0.2
+        constraints.weightx = 0.1
         panel.add(button, constraints)
 
         frame.add(panel, BorderLayout.NORTH)
@@ -59,25 +65,25 @@ class GCAnalyzer {
         frame.setExtendedState((int) frame.getExtendedState() | JFrame.MAXIMIZED_BOTH)
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
         frame.setVisible(true)
-
-
         new Thread({
-
-            Thread.sleep(100)
-
-            SwingUtilities.invokeLater {
-                args.each { String filename ->
-                    def file = new File(filename)
-                    if (file.isDirectory()) {
-                        file.eachFile { File childFile ->
-                            addFile(childFile.absolutePath)
-                        }
+            def files = []
+            args.each { String filename ->
+                def file = new File(filename)
+                if (file.isDirectory()) {
+                    file.eachFile(FileType.FILES) { File childFile ->
+                        files << childFile.absolutePath
                     }
-                    else
-                        addFile(filename)
                 }
-            } as Runnable
-
+                else
+                    files << filename
+            }
+            if (files.size() > 0) {
+                frame.setTitle("$TITLE - Parsing input")
+                counter.set(files.size())
+                files.each {
+                    addFile(it)
+                }
+            }
         } as Runnable).run()
     }
 
@@ -87,7 +93,13 @@ class GCAnalyzer {
         boolean markFullGCs = true
         graphTabs.add('non-PermGen GC', new ChartPanel(gcEventsInformation.getNonPerGenGCChart(markFullGCs)))
         graphTabs.add('event timings', new ChartPanel(gcEventsInformation.getEventTimingsChart(markFullGCs)))
-        fileTabs.add(new File(fileName).name, graphTabs)
+        SwingUtilities.invokeLater {
+            fileTabs.add(new File(fileName).name, graphTabs)
+            if (counter.decrementAndGet() == 0) {
+                frame.setTitle(TITLE)
+                println "changing title to $TITLE"
+            }
+        } as Runnable
     }
 
 }
