3bcadefbaccc653e9fd70fe2d0b505131817fc67
Milan Aleksic <milanaleksic@gmail.com>
2012-02-18 15:43:33 +0100

    copyright header removed

--- src/net/milanaleksic/gcanalyzer/parser/GCSurvivorDetails.groovy
+++ src/net/milanaleksic/gcanalyzer/parser/GCSurvivorDetails.groovy
@@ -4,13 +4,6 @@ import groovy.transform.Immutable
 import javax.annotation.Nullable
 import javax.annotation.Nonnull
 
-/**
- * Created by IntelliJ IDEA.
- * User: b25791
- * Date: 2/15/12
- * Time: 3:16 PM
- * To change this template use File | Settings | File Templates.
- */
 @Immutable
 class GCSurvivorDetails {
 
